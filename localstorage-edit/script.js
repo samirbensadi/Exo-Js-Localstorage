b440c020d393ddba4e8a58c70a6df854ae0f84c2
samir <samirbensadi@gmail.com>
2016-12-02 18:20:26 +0100

    02/12/2016.18h20

--- localstorage-edit/script.js
+++ localstorage-edit/script.js
@@ -1,8 +1,26 @@
 function ecrire() {
-
-	var objet = {
+//on range les valeurs des champs dans la variable objet
+	var objet = [
 		nom = document.getElementById('name').value,
 		valeur = document.getElementById('valeur').value,
-	};
-	alert(objet);
+	];
+	sessionStorage.setItem('objet', objet); //on stock les variable dans le LocalStorage
+	alert("les valeurs: " + objet + " on été stocké dans le Storage"); // on affiche un message de confirmation
 }
+
+function lire() {
+	//on cible la prevZone dans laquelle on va injecter au click les données du local storage. 
+	document.getElementById('readZone').innerHTML = sessionStorage.getItem('objet'); 
+
+}
+
+// zone à travailler : 
+function eff() {
+	 sessionStorage.clear('valeur');
+	 document.getElementById('readZone').innerHTML = sessionStorage.getItem('objet');
+}
+
+function init() {
+	window.location.reload();
+}
+	 
